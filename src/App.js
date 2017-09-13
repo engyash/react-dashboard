accf827d289c3110e3149f627ab486a6a2e81499
Yashwant Vaishnav <vaishnav.yashwant1989@gmail.com>
2017-09-13 11:44:49 +0530

    login screen

--- src/App.js
+++ src/App.js
@@ -3,18 +3,55 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    render() {
+
+        var List = React.createClass({
+            render: function () {
+                return (<div>
+                    {this.props.data.map(function (item) {
+                        return <div className="Team">
+                            <div className="Team-icon">{item.name}</div>
+                            <div className="Team-name-wrapper">
+                                <div className="Team-name">{item.name}<br/></div>
+                                <div className="Team-name-small">{item.name.replace(/ /g, "-")}</div>
+                            </div>
+                                  <div className="Team-notifications">{item.notifications} Notifications!</div>
+                        </div>
+                    })
+                    }
+                </div>);
+            }
+        });
+
+        var data = [{ name: 'Pink Elephant', url: 'https://www.upwork.com', notifications: 15 }, { name: 'Cognits', url: 'http://www.cognits.com', notifications: 5 }];
+
+
+      return (
+
+          <div className="Login-wrapper">
+              <div className="Login-container">
+                  <div className="Login-title">
+                      Which team do you want to Log In?
+                  </div>
+                  <div className="Teams">
+                      <div>Your Teams</div>
+                      <div>
+                          <List data={data} />
+                          <div className="Team">
+                              <div className="Team-icon"><i>+</i></div>
+                               <div className="New-team-name">Create  New Team</div>
+                          </div>
+
+                      </div>
+                  </div>
+                  <div className="SignOut-wrapper">
+                      <input type="button" className="btn btn-default btn-signout" value="Sign Out" />
+                  </div>
+              </div>
+          </div>
+      );
+
+
   }
 }
 
